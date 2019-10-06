e27141a26be1ce3356a1f1543e887fb4e3596c72
Cai heng <ianyaheng822@gmail.com>
2019-10-06 20:23:23 +1100

    finish implementtion of blogs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,51 @@
 class PostsController < ApplicationController
 
   def index
+    @post=Post.all.order("created_at DESC")
     
   end
 
   def new
+    @post=Post.new
     
   end
 
   def create
-    
+    @post=Post.new(post_parmas)
+     if @post.save
+      redirect_to @post
+     else
+      render 'new'
+     end
+  end
+
+  def show
+    @post=Post.find(params[:id])
+  end
+
+  def update
+    @post=Post.find(params[:id])
+    if @post.update(post_parmas)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+
+  def edit
+    @post=Post.find(params[:id])
+  end
+
+  def destroy
+    @post=Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+
+  private
+  def post_parmas
+    params.require(:post).permit(:title,:content)
   end
 end
